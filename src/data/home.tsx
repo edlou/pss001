96aa6cbc73c63962a5badbd1249e190545d0f091
Eddie Lou <eddie@efx-design.com>
2025-09-16 13:30:29 -0700

    Updated site name to code

--- src/data/home.tsx
+++ src/data/home.tsx
@@ -1,6 +1,14 @@
 import { ReactNode } from 'react';
 
-export type SiteKey = 'pss001' | 'client001' | 'efx' | 'missionSmogTestOnlyCenter' | 'fastNCheapSmogStation' |'justSmog' | 'wintonSmogExpress' |'jacksonSmogTestOnlyCentre';
+export type SiteKey =
+  | 'pss001' // Phan's Smog Station
+  | 'mst001' // Mission Smog Test Only Center
+  | 'fnc001' // Fast N Cheap Smog Station
+  | 'jus001' // Just Smog
+  | 'wse001' // Winton Smog Express
+  | 'jst001' // Jackson Smog Test Only Centre, Inc
+  | 'client001' // Client Template
+  | 'efx'; // EFX Template
 
 export interface TestimonialData {
   quote: string;
@@ -70,8 +78,8 @@ export const siteContent: Record<SiteKey, SiteContent> = {
       },
     ],
   },
-  missionSmogTestOnlyCenter:{
-    businessName: "Mission Smog Test Only Center",
+  mst001: {
+    businessName: 'Mission Smog Test Only Center',
     street1: '27159 Mission Blvd ,',
     locality: 'Hayward',
     region: 'CA',
@@ -81,23 +89,39 @@ export const siteContent: Record<SiteKey, SiteContent> = {
     headerName: <>Mission Smog Test Only Center</>,
     footerEntityName: 'Mission Smog Test Only Center',
     googleMap: (
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d295.98126341791396!2d-122.06170047259127!3d37.64336705486582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f940d4b12a71d%3A0x39596e7647fe0dd0!2sMission%20Smog%20Test%20Only!5e0!3m2!1sen!2sus!4v1757970256893!5m2!1sen!2sus"
-      width="100%"
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d295.98126341791396!2d-122.06170047259127!3d37.64336705486582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f940d4b12a71d%3A0x39596e7647fe0dd0!2sMission%20Smog%20Test%20Only!5e0!3m2!1sen!2sus!4v1757970256893!5m2!1sen!2sus"
+        width="100%"
         height="100%"
         style={{ border: 0 }}
         allowFullScreen
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade">
-      </iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     ),
     testimonials: [
-      { quote: 'Great experience coming here.Very good customer service. Very quick and reasonable price', author: 'Krunal Rathod', date: '2025' },
-      { quote: 'Been a loyal customer for 10 years. Always top notch quality service. Very honest. Great business.', author: 'Carl Douglas', date: '2025' },
-      { quote: 'I came in for a smog test. They were super quick and actually went above and beyond to actually help me get my registration figured out as well. I would definitely go to them and refer all my friends and family to them', author: 'Sai Devana', date: '2025' },
+      {
+        quote:
+          'Great experience coming here.Very good customer service. Very quick and reasonable price',
+        author: 'Krunal Rathod',
+        date: '2025',
+      },
+      {
+        quote:
+          'Been a loyal customer for 10 years. Always top notch quality service. Very honest. Great business.',
+        author: 'Carl Douglas',
+        date: '2025',
+      },
+      {
+        quote:
+          'I came in for a smog test. They were super quick and actually went above and beyond to actually help me get my registration figured out as well. I would definitely go to them and refer all my friends and family to them',
+        author: 'Sai Devana',
+        date: '2025',
+      },
     ],
   },
-  fastNCheapSmogStation:{
-    businessName: "Fast N Cheap Smog Station",
+  fnc001: {
+    businessName: 'Fast N Cheap Smog Station',
     street1: '727 Industrial Pkwy W Ste N1,',
     locality: 'Hayward',
     region: 'CA',
@@ -107,22 +131,39 @@ export const siteContent: Record<SiteKey, SiteContent> = {
     headerName: <>Fast N Cheap Smog Station</>,
     footerEntityName: 'Fast N Cheap Smog Station',
     googleMap: (
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.136146472805!2d-122.05933832296711!3d37.62248517202418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f951a3c24d141%3A0xf923bc7c8d44019f!2sFast%20n%20cheap%20smog!5e0!3m2!1sen!2sus!4v1757971229470!5m2!1sen!2sus"    width="100%"
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.136146472805!2d-122.05933832296711!3d37.62248517202418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f951a3c24d141%3A0xf923bc7c8d44019f!2sFast%20n%20cheap%20smog!5e0!3m2!1sen!2sus!4v1757971229470!5m2!1sen!2sus"
+        width="100%"
         height="100%"
         style={{ border: 0 }}
         allowFullScreen
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade">
-      </iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     ),
     testimonials: [
-      { quote: 'One of the best smog shops in the bay. Very knowledgeable and service is amazing, they go above and beyond. Great value for smog too!', author: 'JB', date: '2025' },
-      { quote: 'Technician made the effort to complete the work as soon as he could and was very friendly to customers.', author: 'Joseph M', date: '2025' },
-      { quote: 'Like the name says, it was fast n cheap. Had the best price nearby and good service. Would recommend.', author: 'Faisal A', date: '2025' },
+      {
+        quote:
+          'One of the best smog shops in the bay. Very knowledgeable and service is amazing, they go above and beyond. Great value for smog too!',
+        author: 'JB',
+        date: '2025',
+      },
+      {
+        quote:
+          'Technician made the effort to complete the work as soon as he could and was very friendly to customers.',
+        author: 'Joseph M',
+        date: '2025',
+      },
+      {
+        quote:
+          'Like the name says, it was fast n cheap. Had the best price nearby and good service. Would recommend.',
+        author: 'Faisal A',
+        date: '2025',
+      },
     ],
   },
-  justSmog:{
-    businessName: "Just Smog",
+  jus001: {
+    businessName: 'Just Smog',
     street1: '909 W A St, ',
     locality: 'Hayward',
     region: 'CA',
@@ -132,23 +173,39 @@ export const siteContent: Record<SiteKey, SiteContent> = {
     headerName: <> Just Smog </>,
     footerEntityName: 'Just Smog',
     googleMap: (
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d523.2775529862439!2d-122.11684517798768!3d37.66610616319876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f910f9cad52c7%3A0x62878828bbd483bd!2sJust%20Smog!5e0!3m2!1sen!2sus!4v1757972309886!5m2!1sen!2sus"
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d523.2775529862439!2d-122.11684517798768!3d37.66610616319876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f910f9cad52c7%3A0x62878828bbd483bd!2sJust%20Smog!5e0!3m2!1sen!2sus!4v1757972309886!5m2!1sen!2sus"
         width="100%"
         height="100%"
         style={{ border: 0 }}
         allowFullScreen
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade">
-      </iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     ),
     testimonials: [
-      { quote: 'Located on A Street, easy to miss if you arent from the area. Fast and easy service.., I came on a Friday, 1 person ahead of me. def take ur car here smog test. great pricing too.', author: 'Lyndah T', date: '2025' },
-      { quote: 'My dad brought our old 1989 grand marquis here when I was a kid. Now I get to bring my cars here. No nonsense. Just smog.', author: 'Antonio G', date: '2025' },
-      { quote: 'Lightning fast service. Technician was very friendly and personable. The smog check was very reasonably priced. Will come back again in the future.', author: 'Sandy D', date: '2025' },
+      {
+        quote:
+          'Located on A Street, easy to miss if you arent from the area. Fast and easy service.., I came on a Friday, 1 person ahead of me. def take ur car here smog test. great pricing too.',
+        author: 'Lyndah T',
+        date: '2025',
+      },
+      {
+        quote:
+          'My dad brought our old 1989 grand marquis here when I was a kid. Now I get to bring my cars here. No nonsense. Just smog.',
+        author: 'Antonio G',
+        date: '2025',
+      },
+      {
+        quote:
+          'Lightning fast service. Technician was very friendly and personable. The smog check was very reasonably priced. Will come back again in the future.',
+        author: 'Sandy D',
+        date: '2025',
+      },
     ],
   },
-  wintonSmogExpress:{
-    businessName: "Winton Smog Express",
+  wse001: {
+    businessName: 'Winton Smog Express',
     street1: '1418 W Winton Ave, ',
     locality: 'Hayward',
     region: 'CA',
@@ -165,17 +222,32 @@ export const siteContent: Record<SiteKey, SiteContent> = {
         style={{ border: 0 }}
         allowFullScreen
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade">
-      </iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     ),
     testimonials: [
-      { quote: 'I needed a smog check for a private party vehicle purchase, and came here. Very fast and friendly sevice, clean waiting area, and good price. This store is head and shoulders above other operations I\'ve used in Hayward.', author: 'Kurt B', date: '2025' },
-      { quote: 'Great service and friendly atmosphere! SMOG check was reasonably priced and done quickly.', author: 'Jerome B.', date: '2025' },
-      { quote: 'Brought my sister\'s car here and he took care of it instantly and with great explanation for some other minor issues as well. Great guy and great service.', author: 'Hamid A.', date: '2025' },
+      {
+        quote:
+          "I needed a smog check for a private party vehicle purchase, and came here. Very fast and friendly sevice, clean waiting area, and good price. This store is head and shoulders above other operations I've used in Hayward.",
+        author: 'Kurt B',
+        date: '2025',
+      },
+      {
+        quote:
+          'Great service and friendly atmosphere! SMOG check was reasonably priced and done quickly.',
+        author: 'Jerome B.',
+        date: '2025',
+      },
+      {
+        quote:
+          "Brought my sister's car here and he took care of it instantly and with great explanation for some other minor issues as well. Great guy and great service.",
+        author: 'Hamid A.',
+        date: '2025',
+      },
     ],
   },
-  jacksonSmogTestOnlyCentre:{
-    businessName: "Jackson Smog Test Only Centre, Inc",
+  jst001: {
+    businessName: 'Jackson Smog Test Only Centre, Inc',
     street1: '402 Jackson St , ',
     locality: 'Hayward',
     region: 'CA',
@@ -192,13 +264,28 @@ export const siteContent: Record<SiteKey, SiteContent> = {
         style={{ border: 0 }}
         allowFullScreen
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade">
-      </iframe>
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     ),
     testimonials: [
-      { quote: 'Great service..super fast and easy. He definitely treats you well and I haven\'t been elsewhere for all my smogs he honest and price is very fair', author: 'Nikki S.', date: '2025' },
-      { quote: 'Good Service and very honest. For the service I received, the price very reasonable. I had issue where the model 2019 Honda Odyssey didn\'t pass the Smog check because we had change battery one time but need special way to reset. Some people said to drive on freeway for 20-30 min at 55-65 miles per hour to have the system reset, but I tired so many time and didn\'t work. Left the car for him. Just one night he was able to pass the Smog check for me. It\'s best to call him to be sure he is there and not too busy. He is one man show and need to take care his mom so please contact him before arrival. I really recommend this place. He is very polite and honest. You won\'t be disappointed!', author: 'Nancy L..', date: '2025' },
-      { quote: 'Need a smog , need service on your car, need someone you can trust? Doesn\'t over charge..Look no further', author: 'Pat M.', date: '2025' },
+      {
+        quote:
+          "Great service..super fast and easy. He definitely treats you well and I haven't been elsewhere for all my smogs he honest and price is very fair",
+        author: 'Nikki S.',
+        date: '2025',
+      },
+      {
+        quote:
+          "Good Service and very honest. For the service I received, the price very reasonable. I had issue where the model 2019 Honda Odyssey didn't pass the Smog check because we had change battery one time but need special way to reset. Some people said to drive on freeway for 20-30 min at 55-65 miles per hour to have the system reset, but I tired so many time and didn't work. Left the car for him. Just one night he was able to pass the Smog check for me. It's best to call him to be sure he is there and not too busy. He is one man show and need to take care his mom so please contact him before arrival. I really recommend this place. He is very polite and honest. You won't be disappointed!",
+        author: 'Nancy L..',
+        date: '2025',
+      },
+      {
+        quote:
+          "Need a smog , need service on your car, need someone you can trust? Doesn't over charge..Look no further",
+        author: 'Pat M.',
+        date: '2025',
+      },
     ],
   },
   client001: {
